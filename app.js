4e7b452cc0ff7ebb5f0bea1bf2d2dd66e655787f
jonathan_fambro <startek591@gmail.com>
2023-11-30 16:37:22 -0500

    Matchers

--- app.js
+++ app.js
@@ -12,4 +12,14 @@
       expect(a).toBe(true);
     });
   });
+
+  describe("The 'toBe' matcher compares with ===", function () {
+    it('and has a positive case', function () {
+      expect(true).toBe(true);
+    });
+
+    it('and can have a negative case', function () {
+      expect(false).not.toBe(true);
+    });
+  });
 })();

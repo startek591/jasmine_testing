07717f856b916bdba5502e22cba46adde98597e7
jonathan_fambro <startek591@gmail.com>
2023-11-30 16:49:13 -0500

    The this keyword - another way to share variables

--- app.js
+++ app.js
@@ -44,4 +44,24 @@
 
     xit(); // Setup and Teardown
   });
+
+  describe('A spec', function () {
+    beforeEach(function () {
+      this.foo = 0;
+    });
+
+    it('can use the `this` to share state', function () {
+      expect(this.foo).toEqual(0);
+      this.bar = 'test pollution?';
+    });
+
+    it(
+      'prevents test pollution by having an empty `this` ' +
+        'created for the next spec',
+      function () {
+        expect(this.foo).toEqual(0);
+        expect(this.bar).toBe(undefined);
+      }
+    );
+  });
 })();

dcfd1308f6eadd6b07405d40d04f13fddbf6ed07
jonathan_fambro <startek591@gmail.com>
2023-11-30 20:31:48 -0500

    Disabling suites

--- app.js
+++ app.js
@@ -112,4 +112,17 @@
       });
     });
   });
+
+  xdescribe('A spec', function () {
+    let foo;
+
+    beforeEach(function () {
+      foo = 0;
+      foo += 1;
+    });
+
+    it('is just a function, so it can contain any code', function () {
+      expect(foo).toEqual(1);
+    });
+  });
 })();

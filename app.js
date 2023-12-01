ccd5f73aacecca6715aae39d4f680347d3aeab9a
jonathan_fambro <startek591@gmail.com>
2023-11-30 21:10:51 -0500

    Jasmine.objectContaining

--- app.js
+++ app.js
@@ -248,5 +248,44 @@
         });
       });
     });
+
+    describe('jasmine.objectContaining', function () {
+      let foo;
+
+      beforeEach(function () {
+        foo = {
+          a: 1,
+          b: 2,
+          bar: 'baz',
+        };
+      });
+
+      it('matches objects with the expect key/value paris', function () {
+        expect(foo).toEqual(
+          jasmine.objectContaining({
+            bar: 'baz',
+          })
+        );
+        expect(foo).not.toEqual(
+          jasmine.objectContaining({
+            c: 37,
+          })
+        );
+      });
+
+      describe('when used with a spy', function () {
+        it('is useful for comparing arguments', function () {
+          const callback = jasmine.createSpy('callback');
+
+          callback({
+            bar: 'baz',
+          });
+
+          expect(callback).toHaveBeenCalledWith(
+            jasmine.objectContaining({ bar: 'baz' })
+          );
+        });
+      });
+    });
   });
 })();

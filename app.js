1a0cc5b8a3036392821574820212a8f097bf5f05
jonathan_fambro <startek591@gmail.com>
2023-11-30 20:52:39 -0500

    Create Spy Object

--- app.js
+++ app.js
@@ -185,4 +185,28 @@
       expect(whatAmI).toHaveBeenCalled();
     });
   });
+
+  describe('Multiple spies, when created manually', function () {
+    let tape;
+
+    beforeEach(function () {
+      tape = jasmine.createSpyObj('tape', [
+        'play',
+        'pause',
+        'stop',
+        'rewind',
+      ]);
+
+      tape.play();
+      tape.pause();
+      tape.rewind(0);
+    });
+
+    it('creates spies for each requested function', function () {
+      expect(tape.play).toBeDefined();
+      expect(tape.pause).toBeDefined();
+      expect(tape.stop).toBeDefined();
+      expect(tape.rewind).toBeDefined();
+    });
+  });
 })();

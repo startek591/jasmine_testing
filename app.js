25682e8021945f109a3d5f86d97e1811513f4d0a
jonathan_fambro <startek591@gmail.com>
2023-11-30 21:44:21 -0500

    Mocking the Javascript Timeout Functions

--- app.js
+++ app.js
@@ -340,28 +340,68 @@
           );
         });
       });
+    });
 
-      describe('custom asymmetry', function () {
-        const tester = {
-          asymmetricMatch: function (actual) {
-            const secondValue = actual.split(',')[1];
-            return secondValue;
-          },
-        };
+    describe('custom asymmetry', function () {
+      const tester = {
+        asymmetricMatch: function (actual) {
+          const secondValue = actual.split(',')[1];
+          return secondValue;
+        },
+      };
 
-        it('dives in deep', function () {
-          expect('foo,bar,baz,quux').toEqual(tester);
-        });
+      it('dives in deep', function () {
+        expect('foo,bar,baz,quux').toEqual(tester);
+      });
 
-        describe('when used with a spy', function () {
-          it('is useful for comparing arguments', function () {
-            const callback = jasmine.createSpy('callback');
-            callback('foo,bar,baz');
+      describe('when used with a spy', function () {
+        it('is useful for comparing arguments', function () {
+          const callback = jasmine.createSpy('callback');
+          callback('foo,bar,baz');
 
-            expect(callback).toHaveBeenCalledWith(tester);
-          });
+          expect(callback).toHaveBeenCalledWith(tester);
         });
       });
+    });
+  });
+
+  describe('Manually ticking the Jasmine Clock', function () {
+    let timerCallback;
+
+    beforeEach(function () {
+      timerCallback = jasmine.createSpy('timerCallback');
+      jasmine.clock().install();
+    });
+
+    afterEach(function () {
+      jasmine.clock().uninstall();
+    });
+
+    it('causes a timeout to be called synchronously', function () {
+      setTimeout(function () {
+        timerCallback();
+      }, 100);
+      expect(timerCallback).not.toHaveBeenCalled();
+
+      jasmine.clock().tick(101);
+      expect(timerCallback).toHaveBeenCalled();
+    });
+
+    it('causes an interval to be called synchronously', function () {
+      setInterval(function () {
+        timerCallback();
+      }, 100);
+
+      expect(timerCallback).not.toHaveBeenCalled();
+
+      jasmine.clock().tick(101);
+      expect(timerCallback.calls.count()).toEqual(1);
+
+      jasmine.clock().tick(50);
+      expect(timerCallback.calls.count()).toEqual(1);
+
+      jasmine.clock().tick(50);
+      expect(timerCallback.calls.count()).toEqual(2);
     });
   });
 })();

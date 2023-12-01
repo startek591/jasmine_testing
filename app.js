e1ce47678788fdad569982b670e78c717fdd2c01
jonathan_fambro <startek591@gmail.com>
2023-11-30 20:42:52 -0500

    Spies

--- app.js
+++ app.js
@@ -131,4 +131,44 @@
       expect(true).toBe(false);
     });
   });
+
+  describe('A spy', function () {
+    let foo;
+    let bar = null;
+
+    beforeEach(function () {
+      foo = {
+        setBar: function (value) {
+          bar = value;
+        },
+      };
+
+      spyOn(foo, 'setBar');
+
+      foo.setBar(123);
+      foo.setBar(456, 'another param');
+    });
+
+    it('tracks that the spy was called', function () {
+      expect(foo.setBar).toHaveBeenCalled();
+    });
+
+    it('tracks that the spy was called x times', function () {
+      expect(foo.setBar).toHaveBeenCalledTimes(2);
+    });
+
+    it('tracks all the arguments of its calls', function () {
+      expect(foo.setBar).toHaveBeenCalledWith(123);
+      expect(foo.setBar).toHaveBeenCalledWith(456, 'another param');
+    });
+
+    it('stops all execution on a function', function () {
+      expect(bar).toBeNull();
+    });
+
+    it('tracks if it was called at all', function () {
+      foo.setBar();
+      expect(foo.setBar.calls.any()).toEqual(true);
+    });
+  });
 })();

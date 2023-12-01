7d16e71a3bcbdb98f4373affaf3936b8f4171d80
jonathan_fambro <startek591@gmail.com>
2023-11-30 21:02:26 -0500

    Jasmine.anything

--- app.js
+++ app.js
@@ -231,5 +231,22 @@
         });
       });
     });
+
+    describe('jasmine.anything', function () {
+      it('matches anything', function () {
+        expect(1).toEqual(jasmine.anything());
+      });
+
+      describe('when used with a spy', function () {
+        it('is useful when the argument can be ignored', function () {
+          const foo = jasmine.createSpy('foo');
+          foo(12, function () {
+            return false;
+          });
+
+          expect(foo).toHaveBeenCalledWith(12, jasmine.anything());
+        });
+      });
+    });
   });
 })();

04b2e71eaf11205b72183161d3a507f98441a3f1
jonathan_fambro <startek591@gmail.com>
2023-11-30 20:58:12 -0500

    Matching with more finesse with jasmine.any

--- app.js
+++ app.js
@@ -209,4 +209,27 @@
       expect(tape.rewind).toBeDefined();
     });
   });
+
+  describe('Matching with finesse', function () {
+    describe('jasmine.any', function () {
+      it('matches any value', function () {
+        expect({}).toEqual(jasmine.any(Object));
+        expect(12).toEqual(jasmine.any(Number));
+      });
+
+      describe('when used with a spy', function () {
+        it('is useful for comparing arguments', function () {
+          const foo = jasmine.createSpy('foo');
+          foo(12, function () {
+            return true;
+          });
+
+          expect(foo).toHaveBeenCalledWith(
+            jasmine.any(Number),
+            jasmine.any(Function)
+          );
+        });
+      });
+    });
+  });
 })();

02b10fa458e5cda9b461323c0b8cb00274f12da5
jonathan_fambro <startek591@gmail.com>
2023-11-30 21:24:50 -0500

    jasmine.stringMatching

--- app.js
+++ app.js
@@ -315,5 +315,31 @@
         });
       });
     });
+
+    describe('jasmine.stringMatching', function () {
+      it('matches as a regexp', function () {
+        expect({ foo: 'bar' }).toEqual({
+          foo: jasmine.stringMatching(/^bar$/),
+        });
+        expect({ foo: 'foobarbaz' }).toEqual({
+          foo: jasmine.stringMatching('bar'),
+        });
+      });
+
+      describe('when used with a spy', function () {
+        it('is useful for comparing arguments', function () {
+          const callback = jasmine.createSpy('callback');
+
+          callback('foobarbaz');
+
+          expect(callback).toHaveBeenCalledWith(
+            jasmine.stringMatching('bar')
+          );
+          expect(callback).not.toHaveBeenCalledWith(
+            jasmine.stringMatching(/^bar$/)
+          );
+        });
+      });
+    });
   });
 })();

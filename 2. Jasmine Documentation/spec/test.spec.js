49e1eeb5daefa0d2c75081f3b980b678c830d34d
jonathan_fambro <startek591@gmail.com>
2024-11-08 16:02:12 -0500

    Nesting describe Blocks

--- 2. Jasmine Documentation/spec/test.spec.js
+++ 2. Jasmine Documentation/spec/test.spec.js
@@ -80,3 +80,36 @@ describe('A spec using the fail function', function () {
     });
   });
 });
+
+describe('A spec', function () {
+  let foo;
+
+  beforeEach(function () {
+    foo = 0;
+    foo += 1;
+  });
+
+  afterEach(function () {
+    foo = 0;
+  });
+
+  it('is just a function, so it can contain any code', function () {
+    expect(foo).toEqual(1);
+  });
+
+  it('can have more than one expectation', function () {
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+
+  describe('nested inside a second describe', function () {
+    let bar;
+
+    beforeEach(function () {
+      bar = 1;
+    });
+    it('can reference both scopes as needed', function () {
+      expect(foo).toEqual(bar);
+    });
+  });
+});

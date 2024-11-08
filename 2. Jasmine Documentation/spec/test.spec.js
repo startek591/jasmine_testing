450b50251a0dc180bdf3f1b87386c802f299389c
jonathan_fambro <startek591@gmail.com>
2024-11-08 15:11:33 -0500

    A spec is just a function

--- 2. Jasmine Documentation/spec/test.spec.js
+++ 2. Jasmine Documentation/spec/test.spec.js
@@ -3,3 +3,13 @@ describe('A suite', function () {
     expect(true).toBe(true);
   });
 });
+
+describe('A suite is just a function', function () {
+  let a;
+
+  it('and so is a spec', function () {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});

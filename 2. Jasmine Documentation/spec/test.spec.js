89f32aeb406bde6a177eebc930f3bd0559fb1b57
jonathan_fambro <startek591@gmail.com>
2024-11-08 17:02:51 -0500

    A spy when created manually

--- 2. Jasmine Documentation/spec/test.spec.js
+++ 2. Jasmine Documentation/spec/test.spec.js
@@ -177,3 +177,17 @@ describe('A spy', function () {
     expect(foo.setBar.calls.any()).toEqual(true);
   });
 });
+
+describe('A spy, when created manually', function () {
+  let whatAmI;
+
+  beforeEach(function () {
+    whatAmI = jasmine.createSpy('whatAmI');
+
+    whatAmI('I', 'am', 'a', 'spy');
+  });
+
+  it('tracks that the spy was called', function () {
+    expect(whatAmI).toHaveBeenCalled();
+  });
+});

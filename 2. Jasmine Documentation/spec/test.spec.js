1ba46c43d642d6347a89bffb100deafd52622ea8
jonathan_fambro <startek591@gmail.com>
2024-11-08 16:12:54 -0500

    Pending Specs

--- 2. Jasmine Documentation/spec/test.spec.js
+++ 2. Jasmine Documentation/spec/test.spec.js
@@ -126,3 +126,16 @@ xdescribe('A spec', function () {
     expect(foo).toEqual(1);
   });
 });
+
+describe('Pending specs', function () {
+  xit("can be declared 'xit'", function () {
+    expect(true).toBe(false);
+  });
+
+  it('can be declared with `it` but without a function');
+
+  it("can be declared by calling 'pending' in the spec body", function () {
+    expect(true).toBe(false);
+    pending('this is why it is pending');
+  });
+});

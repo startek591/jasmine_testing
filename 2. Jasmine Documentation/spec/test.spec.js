d146739dc9525a6405f9bdc4ca2ade907cd6923b
jonathan_fambro <startek591@gmail.com>
2024-11-08 20:47:39 -0500

    Jasmine Clock, Mocking the JavaScript Timeout Functions and Date

--- 2. Jasmine Documentation/spec/test.spec.js
+++ 2. Jasmine Documentation/spec/test.spec.js
@@ -336,3 +336,56 @@ describe('custom asymmetry', function () {
     });
   });
 });
+
+describe('Manually ticking the Jasmine Clock', function () {
+  let timerCallback;
+
+  beforeEach(function () {
+    timerCallback = jasmine.createSpy('timerCallback');
+    jasmine.clock().install();
+  });
+
+  afterEach(function () {
+    jasmine.clock().uninstall();
+  });
+
+  it('causes an interval to be called synchronously', function () {
+    setInterval(function () {
+      timerCallback();
+    }, 100);
+
+    expect(timerCallback).not.toHaveBeenCalled();
+
+    jasmine.clock().tick(101);
+
+    expect(timerCallback).toHaveBeenCalled();
+  });
+
+  it('causes an interval to be called synchronously', function () {
+    setInterval(function () {
+      timerCallback();
+    }, 100);
+
+    expect(timerCallback).not.toHaveBeenCalled();
+
+    jasmine.clock().tick(101);
+    expect(timerCallback.calls.count()).toEqual(1);
+
+    jasmine.clock().tick(50);
+    expect(timerCallback.calls.count()).toEqual(1);
+
+    jasmine.clock().tick(50);
+    expect(timerCallback.calls.count()).toEqual(2);
+  });
+
+  describe('Mocking the Date object', function () {
+    it('mocks the Date object and sets it to a given time', function () {
+      const baseTime = new Date(2013, 9, 23);
+
+      jasmine.clock().mockDate(baseTime);
+
+      jasmine.clock().tick(50);
+      expect(new Date().getTime()).toEqual(baseTime.getTime() + 50);
+    });
+  });
+});
